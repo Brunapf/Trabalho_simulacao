1925908ec794f6c37a5d1cf50672126d1a204f48
Brunapf <brunapflor@gmail.com>
2015-11-05 21:41:54 -0200

    primeiro

--- src/dao/DaoFactory.java
+++ src/dao/DaoFactory.java
@@ -1,39 +1,30 @@
 package dao;
 
+import dao.*;
 
-import dao.jpa.ClienteDaoImpl;
 
-import dao.jpa.FuncionarioDaoImpl;
-import dao.jpa.LocacaoDaoImpl;
-import dao.jpa.TipoLocacaoDaoImpl;
-import dao.jpa.VagaDaoImpl;
-import dao.jpa.VeiculoDaoImpl;
+import dao.jpa.BandaDaoImpl;
+import dao.jpa.EspetaculoDaoImpl;
+import dao.jpa.ParticipacaoDaoImpl;
+
 
 
 public class DaoFactory {
 
 		
-	public static ClienteDao criarClienteDao() {
-		return new ClienteDaoImpl();
+	public static BandaDao criarBandaDao() {
+		return new BandaDaoImpl();
 	}
 	
-	public static FuncionarioDao criarFuncionarioDao() {
-		return new FuncionarioDaoImpl();
+	
+	public static EspetaculoDao criarEspetaculoDao() {
+		return new EspetaculoDaoImpl();
 	}
 	
-	public static LocacaoDao criarLocacaoDao() {
-		return new LocacaoDaoImpl();
+	public static ParticipacaoDao criarParticipacaoDao() {
+		return new ParticipacaoDaoImpl();
 	}
 	
-	public static VeiculoDao criarVeiculoDao () {
-		return new VeiculoDaoImpl();
-	}
+
 	
-	public static TipoLocacaoDao criarTipoLocacaoDao() {
-		return new TipoLocacaoDaoImpl();
-	}
-	
-	public static VagaDao criarVagaDao() {
-		return new VagaDaoImpl();
-	}
 }
